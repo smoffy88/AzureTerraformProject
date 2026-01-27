3d9e1dfea8155efc14baa83f5d7f2f96825e8c79
Lee Smith <ljsmith970@gmail.com>
2026-01-27 10:45:11 +0000

    added additional variables

--- variables.tf
+++ variables.tf
@@ -19,4 +19,6 @@ variable "admin_password" {
   description = "Admin password for all VMs"
   type        = string
   sensitive   = true # Hides this in CLI output
+  repository  = "AzureTerraformProject" # GitHub repository name
+  secret_name = "VM_ADMIN_PASSWORD"          # GitHub Secret name
 }
